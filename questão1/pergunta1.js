3860675a3d6901b5fa092f27c9fe31e9cb92d689
MateusBezerraFernandes <115195799+MateusBezerraFernandes@users.noreply.github.com>
2022-10-14 21:01:38 -0300

    Add files via upload

---
+++ questão1/pergunta1.js
@@ -0,0 +1,7 @@
+let  x  =  parseInt ( prompt ( "Digite um valor. Para encerrar, digite 0" ) ) ;
+
+while ( x  !=  0 )  {
+    documento . escreva ( x  *  x ) ;
+    documento . escreva ( "<br>" ) ;
+    x  =  parseInt ( prompt ( "Digite um valor. Para encerrar, digite 0" ) ) ;
+}
